138106847c6df27cc1f6278246df918b35c12933
Aurore Mathon <a.mathon@gmail.com>
2019-09-14 21:34:59 +0200

    CoderJeuxVideo Ch3 Added edge cases handling

--- JeuNombre1.py
+++ JeuNombre1.py
@@ -1,18 +1,30 @@
 # Jeu "Deninez le nombre"
 import random
 
-guesses_taken = 0
-
-print('Bonjour ! Comment t appelles-tu ?')
+print('Bonjour ! Comment t\'appelles-tu ?')
 my_name = input()
 
 number = random.randint(1, 20)
-print('Bien, ' + my_name + ', je pense à un nombre entre 1 et 20.')
+print('Bien, ' + my_name + ', je pense à un nombre entre 1 et 20. Tu as 6 essais.')
 
-for guesses_taken in range(6):
+guesses_taken = 0
+guesses_left = 6
+while guesses_left > 0:
     print('Essaie de le deviner.')
     guess = input()
+
+    if not guess.isdigit():
+        print('Ceci n\'est pas un nombre positif.')
+        continue
+
     guess = int(guess)
+
+    if guess <= 0 or guess > 20:
+        print('Ce nombre n\'est pas entre 1 et 20.')
+        continue
+
+    guesses_left -= 1
+    guesses_taken += 1
 
     if guess < number:
         print('Trop petit.')
@@ -24,9 +36,9 @@ for guesses_taken in range(6):
         break
 
 if guess == number:
-    guesses_taken = str(guesses_taken + 1)
+    guesses_taken = str(guesses_taken)
     print('Bravo, ' + my_name + '! Tu as trouvé mon nombre en ' + guesses_taken +' essai(s) !')
 
 if guess != number:
     number = str(number)    
-    print('Raté ! Le nombre auquel je pensais était' + number + '.')
+    print('Raté ! Le nombre auquel je pensais était ' + number + '.')
